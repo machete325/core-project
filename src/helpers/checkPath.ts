e110cc55aefb40e629e74fde503add24466f544e
machete325 <machete325@gmail.com>
2022-08-25 18:27:16 +0300

    modify checkPath

--- src/helpers/checkPath.ts
+++ src/helpers/checkPath.ts
@@ -1,6 +1,14 @@
-export default function checkPath(linkPath: string, pathname: string) {
-  if (pathname === '/main/' && linkPath === 'projects') {
+export default function checkPath(
+  linkPath: string,
+  pathname: string,
+  path: string,
+  link: string,
+  id?: number | undefined,
+) {
+  if (pathname === path && linkPath === link) {
     return true;
   }
-  return pathname === `/main/${linkPath}`;
+  const linkId = `${id}/`;
+  console.log(pathname, `${path}${id ? linkId : ''}${linkPath}`);
+  return pathname === `${path}${id ? linkId : ''}${linkPath}`;
 }
