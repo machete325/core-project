f4b8e363959e77b37b67d4d9d6971bb78ab448ce
machete325 <machete325@gmail.com>
2022-08-29 17:56:46 +0300

    Use React.Lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ProjectsContainer from './Pages/Projects/ProjectsContainer';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
 import './App.scss';
-import MainContainer from './Pages/Main/MainContainer';
-import ProjectContainer from './Pages/Project/ProjectContainer';
+
+const ProjectsContainer = React.lazy(() => import('./Pages/Projects/ProjectsContainer'));
+const ProjectContainer = React.lazy(() => import('./Pages/Project/ProjectContainer'));
+const ProjectExperimentsContainer = React.lazy(
+  () => import('./Pages/Project/Experiments/ExperimentsContainer'),
+);
+const MainContainer = React.lazy(() => import('./Pages/Main/MainContainer'));
 
 function App() {
   return (
@@ -12,7 +18,20 @@ function App() {
         <Routes>
           <Route path="/">
             <Route index element={<MainContainer />} />
-            <Route path="main" element={<MainContainer />}>
+            <Route
+              path="main"
+              element={(
+                <Suspense
+                  fallback={(
+                    <Backdrop open>
+                      <CircularProgress color="inherit" />
+                    </Backdrop>
+                  )}
+                >
+                  <MainContainer />
+                </Suspense>
+              )}
+            >
               <Route index element={<ProjectsContainer />} />
               <Route path="projects" element={<ProjectsContainer />} />
               <Route path="monitoring" element={<div>Monitoring</div>} />
@@ -23,11 +42,37 @@ function App() {
               <Route path="reports" element={<div>Reports</div>} />
             </Route>
             <Route path="project">
-              <Route path=":projectId" element={<ProjectContainer />}>
+              <Route
+                path=":projectId"
+                element={(
+                  <Suspense
+                    fallback={(
+                      <Backdrop open>
+                        <CircularProgress color="inherit" />
+                      </Backdrop>
+                    )}
+                  >
+                    <ProjectContainer />
+                  </Suspense>
+                )}
+              >
                 <Route index element={<div>Overview</div>} />
                 <Route path="overview" element={<div>Overview</div>} />
                 <Route path="monitoring" element={<div>Monitoring</div>} />
-                <Route path="experiments" element={<div>Experiments</div>} />
+                <Route
+                  path="experiments"
+                  element={(
+                    <Suspense
+                      fallback={(
+                        <Backdrop open>
+                          <CircularProgress color="inherit" />
+                        </Backdrop>
+                      )}
+                    >
+                      <ProjectExperimentsContainer />
+                    </Suspense>
+                  )}
+                />
                 <Route path="datasets" element={<div>Datasets</div>} />
                 <Route path="infrastructure" element={<div>Infrastructure</div>} />
                 <Route path="code" element={<div>Code</div>} />
