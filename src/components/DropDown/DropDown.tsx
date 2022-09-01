c3a8d5128d2260cef4cf7bdcd69e6c7f2a3f32fe
machete325 <machete325@gmail.com>
2022-09-01 12:55:42 +0300

    Modify dropdown

--- src/components/DropDown/DropDown.tsx
+++ src/components/DropDown/DropDown.tsx
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
+import useComponentVisible from '../../core/hooks/useComponentVisible';
 import s from './DropDown.module.scss';
 
 function DropDown() {
   const [visible, setVisible] = useState(false);
-
-  const handleClick = () => {
-    setVisible((prev) => !prev);
-  };
+  const { ref } = useComponentVisible(true, setVisible);
 
   const dropDownConfig = [
     {
@@ -24,31 +22,39 @@ function DropDown() {
   ];
 
   return (
-    <>
-      <div role="presentation" className={s.image_container} onClick={handleClick}>
-        <img alt="Dots" src="/images/icons/DotsThree.svg" />
-      </div>
+    <div className={s.wrapper}>
+      {!visible && (
+        <div
+          role="presentation"
+          className={s.image_container}
+          onClick={() => setVisible((prev) => !prev)}
+        >
+          <img alt="Dots" src="/images/icons/DotsThree.svg" />
+        </div>
+      )}
       {visible && (
-        <div className={s.dropdown}>
-          <div className={s.open}>
-            <img alt="open" src="/images/icons/ArrowSquareUpRight.svg" />
-            <span>Open</span>
-          </div>
-          <div className={s.experiment_methods}>
-            {dropDownConfig.map((method) => (
-              <div className={s.method}>
-                <img alt={method.text} src={method.imgSrc} />
-                <span>{method.text}</span>
-              </div>
-            ))}
-          </div>
-          <div className={s.archive}>
-            <img alt="open" src="/images/icons/TrashSimple.svg" />
-            <span>Archive</span>
+        <div className={s.dropdown} ref={ref}>
+          <div className={s.container}>
+            <div className={s.open}>
+              <img alt="open" src="/images/icons/ArrowSquareUpRight.svg" />
+              <span>Open</span>
+            </div>
+            <div className={s.experiment_methods}>
+              {dropDownConfig.map((method) => (
+                <div className={s.method}>
+                  <img alt={method.text} src={method.imgSrc} />
+                  <span>{method.text}</span>
+                </div>
+              ))}
+            </div>
+            <div className={s.archive}>
+              <img alt="open" src="/images/icons/TrashSimple.svg" />
+              <span>Archive</span>
+            </div>
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 }
 
