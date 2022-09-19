93c40d05345d0f74aa29b4f628ff4e5baa11b16c
machete325 <machete325@gmail.com>
2022-09-19 12:08:31 +0300

    Added MachineDetails

---
+++ src/components/MachineDetails/MachineDetails.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import s from './MachineDetails.module.scss';
+
+function MachineDetails() {
+  return (
+    <div className={s.wrapper}>
+      <div className={s.title}>Machine details</div>
+      <div className={s.content}>
+        <div>
+          <img src="/images/machine.svg" alt="machine" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MachineDetails;
