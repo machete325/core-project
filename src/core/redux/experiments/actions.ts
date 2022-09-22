f239b8d4d363a4d733d004a2a50ab1e624afff2d
machete325 <machete325@gmail.com>
2022-09-22 22:03:03 +0300

    Added login function

--- src/core/redux/experiments/actions.ts
+++ src/core/redux/experiments/actions.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { ExperimentService } from '../../services/Experiment.service';
 import { AppThunk, AppDispatch } from '../store';
 import { experimentSlice } from './reducer';
@@ -5,11 +6,29 @@ import { experimentSlice } from './reducer';
 const { setExperiments, startLoading, setCheckExperiments } = experimentSlice.actions;
 const { getProjectExperiments } = ExperimentService;
 
+const loginHandle = async () => {
+  const data = {
+    username: 'evgeny',
+    password: 'coreai-blabla',
+  };
+  const response = await axios({
+    method: 'post',
+    url: 'http://3.126.123.50:8000/api/v1/login',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    withCredentials: true,
+    data,
+  });
+  console.log(response);
+};
+
 export const fetchExperiments = (projectId: string): AppThunk => async (dispatch: AppDispatch) => {
   try {
+    loginHandle();
     dispatch(startLoading);
-    const responce = await getProjectExperiments(projectId, true);
-    dispatch(setExperiments(responce.data.items));
+    const response = await getProjectExperiments(projectId, true);
+    dispatch(setExperiments(response.data.items));
   } catch (e) {
     console.log(e);
   }
