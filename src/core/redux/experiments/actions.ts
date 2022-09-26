ba16a0d7003d86b1a3aaedbe672c9959cf628658
machete325 <machete325@gmail.com>
2022-09-26 18:24:40 +0300

    Delete login handle

--- src/core/redux/experiments/actions.ts
+++ src/core/redux/experiments/actions.ts
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import { ExperimentService } from '../../services/Experiment.service';
 import { AppThunk, AppDispatch } from '../store';
 import { experimentSlice } from './reducer';
@@ -6,26 +5,8 @@ import { experimentSlice } from './reducer';
 const { setExperiments, startLoading, setCheckExperiments } = experimentSlice.actions;
 const { getProjectExperiments } = ExperimentService;
 
-const loginHandle = async () => {
-  const data = {
-    username: 'evgeny',
-    password: 'coreai-blabla',
-  };
-  const response = await axios({
-    method: 'post',
-    url: 'http://3.126.123.50:8000/api/v1/login',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    withCredentials: true,
-    data,
-  });
-  console.log(response);
-};
-
 export const fetchExperiments = (projectId: string): AppThunk => async (dispatch: AppDispatch) => {
   try {
-    loginHandle();
     dispatch(startLoading);
     const response = await getProjectExperiments(projectId, true);
     dispatch(setExperiments(response.data.items));
