657f72bceb40437dda3ed34d5c796267d2f9232b
machete325 <machete325@gmail.com>
2022-09-28 18:22:24 +0300

    rebuild config and logic for experiments

--- src/Pages/Project/Experiments/ExperimentsContainer.tsx
+++ src/Pages/Project/Experiments/ExperimentsContainer.tsx
@@ -13,7 +13,8 @@ import { convertToString } from '../../../core/helpers/convertPath';
 import s from './Experiments.module.scss';
 import ProjectTitle from '../../../components/ProjectTitle/ProjectTitle';
 import ProjectStatus from '../../../components/ProjectStatus/ProjectStatus';
-import StatusTag from '../../../components/StatusTag/StatusTag';
+import experimentConfig from './Experiment.config';
+import updateRecentlyOpened from '../../../core/helpers/updateRecentlyOpened';
 
 const mockProjectData = [
   { id: 'SalesPredictionKaggle', name: 'Demand Forecasting' },
@@ -48,6 +49,7 @@ function ProjectExperimentsContainer() {
 
   const handleOpenModal = (activeTab: string, id: string) => {
     const experiment = data[id];
+    updateRecentlyOpened(experiment.id, 'experiment', experiment.name);
     setOpen(true);
     setChoosedTab({ ...choosedTab, type: activeTab, data: experiment });
   };
@@ -56,194 +58,84 @@ function ProjectExperimentsContainer() {
     setOpen(false);
   };
 
-  const genTableDataMark = (
-    model: Object | string | number,
-    activeTab: string,
-    id: string,
-    isTitle: boolean,
-  ) => {
-    const marking = (key: any, value: any) => {
-      switch (activeTab) {
-        case 'infrastructure':
-          return (
-            <div
-              key={key}
-              role="presentation"
-              onClick={() => handleOpenModal(activeTab, id)}
-              className={s.obj_container}
-            >
-              {value.value}
-            </div>
-          );
-        default:
-          return (
-            <div
-              key={key}
-              role="presentation"
-              onClick={() => handleOpenModal(activeTab, id)}
-              className={typeof model === 'object' ? s.obj_container : ''}
-            >
-              {isTitle && (
-              <span className={s.title_key}>
-                {key}
-                :
-              </span>
-              )}
-              {value || 'null'}
-            </div>
-          );
-      }
-    };
-    const result = typeof model === 'object' && model !== null
-      ? Object.entries(model).map(([key, value]) => marking(key, value))
-      : marking(1, model);
-    return <td key={activeTab}>{result}</td>;
-  };
-
   /* {
       name: name of experiment section,
       path: path to information on experiment data,
       mainInfoFields: fields for showing main information,
-      length: amount of fields for showing
     }
   */
 
-  const experimentConfig = {
-    description: {
-      name: 'Description',
-      path: '.description',
-      mainInfoFields: [],
-      length: undefined,
-      formattingFunction: undefined,
-      isTitle: false,
-    },
-    target: {
-      name: 'Target',
-      path: '.target',
-      mainInfoFields: [],
-      length: undefined,
-      formattingFunction: undefined,
-      isTitle: false,
-    },
-    dataset: {
-      name: 'Data',
-      path: '.dataset.name',
-      mainInfoFields: [],
-      length: undefined,
-      formattingFunction: undefined,
-      isTitle: false,
-    },
-    metrics: {
-      name: 'Main Metrics',
-      path: '.metrics.items',
-      mainInfoFields: ['displayName', 'value'],
-      length: 3,
-      formattingFunction: undefined,
-      isTitle: true,
-    },
-    configuration: {
-      name: 'Model configuration',
-      path: '.configuration.items.runner.models.lightgbm.parameters.hyper_parameters',
-      mainInfoFields: ['displayName', 'value'],
-      length: 3,
-      formattingFunction: undefined,
-      isTitle: true,
-    },
-    infrastructure: {
-      name: 'Infrastructure',
-      path: '.infrastructure',
-      mainInfoFields: [],
-      length: undefined,
-      formattingFunction: (infrastructure: any) => {
-        const tempObj = {
-          trained: { value: <>1</>, isTitle: true },
-          avg: { value: <>1</>, isTitle: true },
-          used: { value: <>1</>, isTitle: false },
-        };
-        tempObj.trained.value = (
-          <>
-            <span className={s.title_key}>Trained in:</span>
-            {` ${infrastructure.trainingTime}`}
-          </>
-        );
-        tempObj.avg.value = (
-          <>
-            <span className={s.title_key}>Avg. cost:</span>
-            <StatusTag
-              usedValue={infrastructure.usedBudget}
-              totalValue={infrastructure.totalBudget}
-            />
-          </>
-        );
-        tempObj.used.value = (
+  const rebuildData = (config: any, id: string) => {
+    // eslint-disable-next-line no-debugger
+    const arr: any = [];
+
+    const markFunction = (formattedData: any, key: any) => {
+      let result = null;
+      const markObj = (value: any, index: any, displayName?: any) => (
+        <div
+          key={index}
+          role="presentation"
+          onClick={() => handleOpenModal(key, id)}
+          className={s.obj_container}
+        >
+          {key !== 'infrastructure' && (
           <span className={s.title_key}>
-            {`Used ${infrastructure.usedMachines}/${infrastructure.totalMachines} VMs`}
+            {displayName}
+            :
           </span>
-        );
-        return tempObj;
-      },
+          )}
+          <span>{value}</span>
+        </div>
+      );
 
-      isTitle: false,
-    },
-    last_commit: {
-      name: 'Commit Description',
-      path: '.code.commitMessage',
-      mainInfoFields: [],
-      length: undefined,
-      formattingFunction: undefined,
-      isTitle: false,
-    },
-  };
+      const markString = (value: any) => (
+        <div key={key} role="presentation" onClick={() => handleOpenModal(key, id)}>
+          {value}
+        </div>
+      );
 
-  const genTableData = (
-    config: {
-      [key: string]: {
-        name: string;
-        path: string;
-        mainInfoFields: any[];
-        length: number | undefined;
-        formattingFunction: ((field: any) => void) | undefined;
-        isTitle: boolean;
-      };
-    },
-    id: string,
-  ) => {
-    const arr: any[] = [];
-    const experimentData = data[id];
-
-    const formatObject = (obj: any, mainInfoFields: string[], length: number | undefined) => {
-      const keys = typeof obj === 'object' ? obj !== null && Object.keys(obj) : false;
-      const isObj = keys && typeof obj[keys[0]] === 'object';
-      if (isObj) {
-        const tempData: { [key: string]: any } = {};
-        Object.keys(obj).forEach((item, index) => {
-          if (typeof length !== 'undefined' && index > length - 1) {
-            return null;
+      if (typeof formattedData === 'string') {
+        result = markString(formattedData);
+      } else {
+        result = Object.entries(formattedData).map(([itemKey, itemValue]: any) => {
+          if (key === 'infrastructure') {
+            return markObj(itemValue.value, itemKey);
           }
-          tempData[obj[item][mainInfoFields[0]]] = obj[item][mainInfoFields[1]];
-          return tempData;
+          return markObj(itemValue.value, itemKey, itemValue.displayName);
         });
-        return tempData;
       }
-      return obj;
+      return <td key={key}>{result}</td>;
     };
 
-    Object.entries(config).forEach((item) => {
-      const [key, value] = item;
-      const tableData = formatObject(
-        convertToString(experimentData, value.path),
-        value.mainInfoFields,
-        value.length,
-      );
-      arr.push(
-        genTableDataMark(
-          value.formattingFunction ? value.formattingFunction(tableData) : tableData,
-          key,
-          id,
-          value.isTitle,
-        ),
-      );
-    });
+    const checkIsExistFormatFunction = (obj: any, objConfig: any) => {
+      let tempArr: any = [];
+      if (objConfig.formattingFunction) {
+        const objData = objConfig.formattingFunction(obj);
+        if (Array.isArray(objData)) {
+          tempArr = objData;
+        } else {
+          Object.keys(objData).forEach((key) => {
+            tempArr.push(objData[key]);
+          });
+        }
+      } else {
+        Object.values(obj).forEach((item) => {
+          tempArr.push(item);
+        });
+      }
+      return tempArr;
+    };
+
+    if (data && Object.keys(data).length !== 0) {
+      const experimentData = data[id];
+      Object.entries(config).forEach(([key, value]: any) => {
+        const currentField = convertToString(experimentData, value.path);
+        const markData = typeof currentField !== 'object' || currentField === null
+          ? `${currentField}`
+          : checkIsExistFormatFunction(currentField, value);
+        arr.push(markFunction(markData, key));
+      });
+    }
     return arr;
   };
 
@@ -296,7 +188,7 @@ function ProjectExperimentsContainer() {
                     <CheckBox id={key} checked={data[key].checked} />
                   </td>
                   <td>{index + 1}</td>
-                  {genTableData(experimentConfig, key)}
+                  {rebuildData(experimentConfig, key)}
                   <td>
                     <ProjectStatus status={data[key].status} />
                   </td>
