0f272f4ff4e00c0bb148c3d7c01e2d99bbd988fe
Yevhenii <machete325@gmail.com>
2022-10-04 13:42:59 +0300

    Remove unnecessary types

--- src/core/redux/experiments/types.ts
+++ src/core/redux/experiments/types.ts
@@ -1,51 +1,3 @@
-export interface IExperimentDataF {
-  version: string;
-  name: string;
-  dataset: {
-    version: string;
-    prefix: string;
-  };
-  project_version: string;
-  description: string;
-  target: string;
-  status: string;
-  is_updatable: boolean;
-  software: string;
-  hardware: string;
-  hostname: string;
-  url: string;
-  url_artifacts: string;
-  url_codebase: string;
-  url_configuration: string;
-  url_debug_samples: string;
-  url_execution: string;
-  url_last_model: string;
-  url_metrics: string;
-  url_project: string;
-  url_report: string;
-  requirements: {
-    software: string;
-    pip: string[];
-    conda: any[];
-  };
-  metrics: { [key: string]: number };
-  infrastructure: {
-    training_time: 5220;
-    average_cost: 200;
-    total_budget: 500;
-    currency: 'USD';
-    used_machines: 2;
-    total_machines: 3;
-  };
-  configuration: { [key: string]: string };
-  commit: {
-    last_commit: {
-      description: string;
-    };
-  };
-  checked: boolean;
-}
-
 interface IMetric {
   id: string;
   name: string;
@@ -53,14 +5,6 @@ interface IMetric {
   display: boolean;
   value: number;
   aggregationOperation: string;
-}
-
-interface IHyperParameters {
-  id: string;
-  name: string;
-  displayName: string;
-  display: boolean;
-  value: number;
 }
 
 export interface IExperiment {
@@ -81,19 +25,7 @@ export interface IExperiment {
     items: { [key: string]: IMetric };
   };
   configuration: {
-    items: {
-      runner: {
-        models: {
-          lightgbm: {
-            parameters: {
-              hyper_parameters: {
-                [key: string]: IHyperParameters;
-              };
-            };
-          };
-        };
-      };
-    };
+    items: any
   };
   infrastructure: {
     trainingTime: number;
