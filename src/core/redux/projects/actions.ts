ac069ee00fa1e2db796392043deab1c9fa6132ee
Yevhenii <machete325@gmail.com>
2022-10-04 13:43:11 +0300

    Added getRecentlyData

--- src/core/redux/projects/actions.ts
+++ src/core/redux/projects/actions.ts
@@ -1,8 +1,9 @@
 import { ProjectService } from '../../services/Project.service';
 import { AppThunk, AppDispatch } from '../store';
 import { projectSlice } from './reducer';
+import { getFromLocalStorage } from '../../helpers/localStorageMethods';
 
-const { setProjects, startLoading } = projectSlice.actions;
+const { setProjects, startLoading, setRecentlyData } = projectSlice.actions;
 const { getAllProjects } = ProjectService;
 
 export const fetchProjects = (): AppThunk => async (dispatch: AppDispatch) => {
@@ -13,5 +14,12 @@ export const fetchProjects = (): AppThunk => async (dispatch: AppDispatch) => {
     dispatch(setProjects(responce.data));
   } catch (e) {
     console.log(e);
+  }
+};
+
+export const getRecentlyData = (projectId: string) => (dispatch: AppDispatch) => {
+  const storageData = getFromLocalStorage('recentlyOpened');
+  if (typeof storageData === 'string' && !!storageData !== false) {
+    dispatch(setRecentlyData(JSON.parse(storageData)[projectId]));
   }
 };
