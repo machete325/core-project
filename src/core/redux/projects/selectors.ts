7ee555df2520c12457b45e9a8726f475d8294389
Yevhenii <machete325@gmail.com>
2022-10-04 13:43:46 +0300

    Added recentlyOpenedData

--- src/core/redux/projects/selectors.ts
+++ src/core/redux/projects/selectors.ts
@@ -1,3 +1,4 @@
 import { RootState } from '../slices';
 
 export const projectsSelector = (state: RootState) => state.projects;
+export const recentlyOpenedData = (state: RootState) => state.projects.recentlyOpenedData;
