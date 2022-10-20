2857b5bd4b0192a5ee772b097bc02ab260679ddf
Yevhenii <machete325@gmail.com>
2022-10-20 19:17:11 +0300

    feat(project.service.tsx): added getProjectOverview
    Added getProjectOverview

--- src/core/services/Project.service.ts
+++ src/core/services/Project.service.ts
@@ -3,6 +3,13 @@ import api from '../api';
 export const ProjectService = {
   getAllProjects: () => api('GET', [], '/projects?page_size=7'),
   getOneProject: (version: string) => api('GET', [], `/projects/${version}`),
+  getProjectOverview: (version: string, display: boolean) => api(
+    'GET',
+    [],
+    `/projects/${version}/overview${
+      display ? '?display=true' : '?display=false'
+    }`,
+  ),
 };
 
 // Project
