fc0b3fbbda59e3f1acadd0186d621cb881c073ff
Yevhenii <machete325@gmail.com>
2022-10-25 19:19:27 +0300

    feat(overview.service): added Overview service
    Added Overview service

--- src/core/services/projects/Experiment.service.ts
+++ src/core/services/projects/Experiment.service.ts
@@ -1,4 +1,4 @@
-import api from '../api';
+import api from '../../api';
 
 const getBaseUrl = (projectVersion: string, version: string) => `/projects/${projectVersion}/experiments/${version}`;
 const getAdditionalParams = (
--- src/core/services/projects/Project.service.ts
+++ src/core/services/projects/Project.service.ts
@@ -1,4 +1,4 @@
-import api from '../api';
+import api from '../../api';
 
 export const ProjectService = {
   getAllProjects: () => api('GET', [], '/projects?page_size=7'),
