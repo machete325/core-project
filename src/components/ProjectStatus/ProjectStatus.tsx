172323740cc2d4cf832fdd6db8ed445b05944554
Yevhenii <machete325@gmail.com>
2022-11-03 11:16:58 +0200

    feat(projectstatus.tsx): added new cases for checkStatus
    Added new cases for checkStatus

--- src/components/ProjectStatus/ProjectStatus.tsx
+++ src/components/ProjectStatus/ProjectStatus.tsx
@@ -9,12 +9,82 @@ function ProjectStatus({ status }: Props) {
   const [state, setState] = useState({
     color: '',
     text: '',
+    textColor: '',
   });
 
   const checkStatus = () => {
     switch (status) {
       case 'completed':
-        return setState({ ...state, color: '#042588', text: 'Done' });
+        return setState({
+          ...state,
+          color: '#042588',
+          text: 'Done',
+          textColor: '#FFFFFF',
+        });
+      case 'running':
+        return setState({
+          ...state,
+          color: '#2DADB5',
+          text: 'Running',
+          textColor: '#FFFFFF',
+        });
+      case 'que':
+      case 'queued':
+        return setState({
+          ...state,
+          color: '#87A4D1',
+          text: 'In que',
+          textColor: '#333333',
+        });
+      case 'stopped':
+        return setState({
+          ...state,
+          color: '#FFD600',
+          text: 'Stopped',
+          textColor: '#333333',
+        });
+      case 'in_progress':
+        return setState({
+          ...state,
+          color: '#1DF580',
+          text: 'Running',
+          textColor: '#333333',
+        });
+      case 'failed':
+        return setState({
+          ...state,
+          color: '#F51D44',
+          text: 'Failed',
+          textColor: '#FFFFFF',
+        });
+      case 'created':
+        return setState({
+          ...state,
+          color: '#005971',
+          text: 'Created',
+          textColor: '#333333',
+        });
+      case 'published':
+        return setState({
+          ...state,
+          color: '#5237FB',
+          text: 'Published',
+          textColor: '#FFFFFF',
+        });
+      case 'publishing':
+        return setState({
+          ...state,
+          color: '#9A86FD',
+          text: 'Publishing',
+          textColor: '#FFFFFF',
+        });
+      case 'unknown':
+        return setState({
+          ...state,
+          color: '#4E4E52',
+          text: 'Unknown',
+          textColor: '#FFFFFF',
+        });
       default:
         return status;
     }
@@ -25,7 +95,10 @@ function ProjectStatus({ status }: Props) {
   }, [status]);
 
   return (
-    <div className={s.button} style={{ backgroundColor: state.color }}>
+    <div
+      className={s.button}
+      style={{ backgroundColor: state.color, color: state.textColor }}
+    >
       {state.text}
     </div>
   );
