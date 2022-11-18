44177eb25a4e5a01e9e1fbabbff823e4096bb12e
Yevhenii <machete325@gmail.com>
2022-11-18 13:05:04 +0200

    feat(navigateprojectitem.tsx): added UI and logic for total count of experiments
    Added UI and logic for total count of experiments

--- src/components/NavigateProjectItem/NavigateProjectItem.tsx
+++ src/components/NavigateProjectItem/NavigateProjectItem.tsx
@@ -5,15 +5,21 @@ type Props = {
   text: string;
   active: boolean;
   imgSrc: string;
+  totalCount: number | undefined;
 };
 
-function NavigateProjectItem({ text, active, imgSrc }: Props) {
+function NavigateProjectItem({
+  text, active, imgSrc, totalCount,
+}: Props) {
   return (
     <div className={active ? s.item_active : s.item}>
-      <div className={s.icon}>
-        <img alt={text} width="30px" src={imgSrc} />
+      <div className={s.navigate}>
+        <div className={s.icon}>
+          <img alt={text} width="30px" src={imgSrc} />
+        </div>
+        <div className={s.text}>{text}</div>
       </div>
-      <div className={s.text}>{text}</div>
+      {totalCount && <div className={s.count}>{totalCount}</div>}
     </div>
   );
 }
