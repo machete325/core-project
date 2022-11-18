7f5942d502a45c0fda96712dd8fbd8261b9d8592
Yevhenii <machete325@gmail.com>
2022-11-18 13:15:01 +0200

    feat(project.service): added uploadProjectPhoto and signal param
    Added uploadProjectPhoto and signal param

--- src/core/services/projects/Project.service.ts
+++ src/core/services/projects/Project.service.ts
@@ -2,15 +2,19 @@ import api from '../../api';
 
 export const ProjectService = {
   getAllProjects: () => api('GET', [], '/projects?page_size=5'),
-  getOneProject: (version: string) => api('GET', [], `/projects/${version}`),
+  getOneProject: (version: string, signal?: AbortSignal) => api('GET', [], `/projects/${version}`, {}, signal),
   getProjectOverview: (version: string, display: boolean, signal: any) => api(
     'GET',
     [],
     `/projects/${version}/overview${
       display ? '?display=true' : '?display=false'
     }`,
+    {},
     signal,
   ),
+  uploadProjectPhoto: (version: string, data: any) => api('POST', data, `/projects/${version}/image`, {
+    'Content-Type': 'multipart/form-data',
+  }),
 };
 
 // Project
