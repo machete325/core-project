a21b0dba706a035911175f5405fd3f63db479592
Yevhenii <machete325@gmail.com>
2022-11-19 18:11:12 +0200

    feat(gettabcontent): adjusted the logic for the dataset
    Adjusted the logic for the dataset

--- src/core/helpers/getTabContent.tsx
+++ src/core/helpers/getTabContent.tsx
@@ -1,34 +1,68 @@
 import React from 'react';
-import Description from '../../Pages/Projects/ProjectPages/Experiments/Description/Description';
-import Target from '../../Pages/Projects/ProjectPages/Experiments/Target/Target';
-import DatasetContainer from '../../Pages/Projects/ProjectPages/Experiments/Dataset/DatasetContainer';
-import MainMetrics from '../../Pages/Projects/ProjectPages/Experiments/MainMetrics/MainMetrics';
-import ModelConfiguration from '../../Pages/Projects/ProjectPages/Experiments/ModelConfiguration/ModelConfiguration';
-import Infrastructure from '../../Pages/Projects/ProjectPages/Experiments/Infrastructure/Infrastructure';
-import CommitDescription from '../../Pages/Projects/ProjectPages/Experiments/CommitDescription/CommitDescription';
+import ExperimentDescription from '../../Pages/Projects/ProjectPages/Experiments/Description/Description';
+import ExperimentTarget from '../../Pages/Projects/ProjectPages/Experiments/Target/Target';
+import ExperimentDatasetContainer from '../../Pages/Projects/ProjectPages/Experiments/Dataset/DatasetContainer';
+import ExperimentMainMetrics from '../../Pages/Projects/ProjectPages/Experiments/MainMetrics/MainMetrics';
+import ExperimentModelConfiguration from '../../Pages/Projects/ProjectPages/Experiments/ModelConfiguration/ModelConfiguration';
+import ExperimentInfrastructure from '../../Pages/Projects/ProjectPages/Experiments/Infrastructure/Infrastructure';
+import ExperimentCommitDescription from '../../Pages/Projects/ProjectPages/Experiments/CommitDescription/CommitDescription';
+import DatasetDescription from '../../Pages/Projects/ProjectPages/Datasets/Overview/Overview';
 import { IProjectData } from '../../components/Modal/types';
 
-export const getTabContent = (
-  tab: { type: string; path: string },
+const returnExperimentTabs = (
+  type: string,
   data: any,
   projectData: IProjectData,
 ) => {
-  switch (tab.type) {
+  switch (type) {
     case 'description':
-      return <Description data={data} projectData={projectData} />;
+      return <ExperimentDescription data={data} />;
     case 'target':
-      return <Target data={data} projectData={projectData} />;
+      return <ExperimentTarget data={data} projectData={projectData} />;
     case 'data':
-      return <DatasetContainer data={data} projectData={projectData} />;
+      return (
+        <ExperimentDatasetContainer data={data} projectData={projectData} />
+      );
     case 'metrics':
-      return <MainMetrics data={data} projectData={projectData} />;
+      return <ExperimentMainMetrics data={data} projectData={projectData} />;
     case 'configuration':
-      return <ModelConfiguration data={data} projectData={projectData} />;
+      return (
+        <ExperimentModelConfiguration data={data} projectData={projectData} />
+      );
     case 'infrastructure':
-      return <Infrastructure data={data} projectData={projectData} />;
+      return <ExperimentInfrastructure data={data} projectData={projectData} />;
     case 'last_commit':
-      return <CommitDescription data={data} projectData={projectData} />;
+      return (
+        <ExperimentCommitDescription data={data} projectData={projectData} />
+      );
     default:
       return null;
   }
+};
+
+const returnDatasetTabs = (
+  type: string,
+  data: any,
+  projectData: IProjectData,
+) => {
+  switch (type) {
+    case 'description':
+      return <DatasetDescription data={data} projectData={projectData} />;
+    default:
+      return null;
+  }
+};
+
+export const getTabContent = (
+  tab: { type: string; path: string; page: string },
+  data: any,
+  projectData: IProjectData,
+) => {
+  if (tab.page === 'experiments') {
+    return returnExperimentTabs(tab.type, data, projectData);
+  }
+  if (tab.page === 'datasets') {
+    return returnDatasetTabs(tab.type, data, projectData);
+  }
+  return '';
 };
