504585ad4946a271a8d8eb0282117b5f027b8770
Yevhenii <machete325@gmail.com>
2022-12-01 21:27:34 +0200

    feat(tobeimplemented.tsx): created ToBeImpelemented
    Created ToBeImpelemented

---
+++ src/components/ToBeImpelemented/ToBeImpelemented.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { styled } from '@mui/material/styles';
+import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
+import s from './ToBeImpelemented.module.scss';
+
+const HtmlTooltip = styled(({ className, ...props }: TooltipProps) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))(() => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    display: 'flex',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f9',
+    color: 'rgba(0, 0, 0, 0.87)',
+    maxWidth: 240,
+    height: '40px',
+    boxSizing: 'border-box',
+    fontSize: '16px',
+    fontFamily: 'Open Sans',
+    fontWeight: '600',
+    border: '1px solid #dadde9',
+  },
+}));
+
+interface Props {
+  element: any;
+  color: 'primary' | 'secondary';
+}
+
+function ToBeImpelemented({ element, color }: Props) {
+  return (
+    <div className={s.wrapper}>
+      <HtmlTooltip
+        placement="top-start"
+        title={<div color="inherit">To be customized</div>}
+      >
+        <div style={{ color: color === 'primary' ? 'white' : 'balck' }}>
+          {element}
+        </div>
+      </HtmlTooltip>
+    </div>
+  );
+}
+
+export default ToBeImpelemented;
