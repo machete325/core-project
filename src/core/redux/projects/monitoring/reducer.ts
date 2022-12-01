ee82150d3a29b47ec7007e319e386f219a07e0b1
Yevhenii <machete325@gmail.com>
2022-12-01 21:29:25 +0200

    feat(monitoring): added monitoring redux-logic
    Added monitoring redux-logic

---
+++ src/core/redux/projects/monitoring/reducer.ts
@@ -0,0 +1,65 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { IMonitoringItems } from '../../../../types/project/monitoring';
+
+export type MonitoringState = {
+  loading: boolean;
+  hasErrors: boolean;
+  fetching: boolean;
+  currentPage: number;
+  totalCount: number | undefined;
+  data: IMonitoringItems;
+};
+//
+export const initialState: MonitoringState = {
+  loading: false,
+  hasErrors: false,
+  fetching: false,
+  currentPage: 0,
+  totalCount: undefined,
+  data: {},
+};
+// A slice
+export const monitoringSlice = createSlice({
+  name: 'projectMonitoring',
+  initialState,
+  reducers: {
+    setMonitoring: (state, action: PayloadAction<any>) => {
+      const data = action.payload;
+      Object.keys(data).forEach((key: string) => {
+        data[key].checked = false;
+      });
+      state.data = { ...state.data, ...action.payload };
+    },
+    startLoading: (state) => {
+      state.loading = true;
+    },
+    finishLoading: (state) => {
+      state.loading = false;
+    },
+    setTotalCount: (state, action: PayloadAction<number>) => {
+      state.totalCount = action.payload;
+    },
+    setCheckAllMonitoring: (state, action: PayloadAction<boolean>) => {
+      Object.keys(state.data).forEach((key: string) => {
+        state.data[key].checked = action.payload;
+      });
+    },
+    setCheckDataset: (state, action: PayloadAction<string>) => {
+      const id = action.payload;
+      state.data[id].checked = !state.data[id].checked;
+    },
+    setFetching: (state, action: PayloadAction<boolean>) => {
+      state.fetching = action.payload;
+    },
+    setCurrentPage: (state, action: PayloadAction<number>) => {
+      state.currentPage = action.payload;
+    },
+    setErrors: (state, action: PayloadAction<boolean>) => {
+      state.hasErrors = action.payload;
+    },
+    clearData: () => initialState,
+  },
+});
+
+const projectMonitoringSliceReducer = monitoringSlice.reducer;
+export default projectMonitoringSliceReducer;
--- src/core/redux/slices.ts
+++ src/core/redux/slices.ts
@@ -3,12 +3,14 @@ import projectReducer from './projects/reducer';
 import projectExperimentReducer from './projects/experiments/reducer';
 import projectOverviewReducer from './projects/overview/reducer';
 import projectDatasetReducer from './projects/datasets/reducer';
+import projectMonitoringSliceReducer from './projects/monitoring/reducer';
 
 const rootReducer = combineReducers({
   projects: projectReducer,
   projectExperiments: projectExperimentReducer,
   projectOverview: projectOverviewReducer,
   projectDatasets: projectDatasetReducer,
+  projectMonitoring: projectMonitoringSliceReducer,
   //  if we need to use more reducers
 });
 export type RootState = ReturnType<typeof rootReducer>;
