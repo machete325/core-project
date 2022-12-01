3d2f7635d0fc90b44cfa707fdf3215ba66bcd514
Yevhenii <machete325@gmail.com>
2022-12-01 21:34:36 +0200

    feat(types): added common types
    Added common types

---
+++ src/types/project/experiments.ts
@@ -0,0 +1,41 @@
+import { IDataset, IExpandDataset } from './datasets';
+import { IInfrastructure } from './project';
+
+interface IMetric {
+  id: string;
+  name: string;
+  displayName: string;
+  display: boolean;
+  value: number;
+  threshold: number;
+  aggregationOperation: string;
+}
+
+export interface IExperiment {
+  id: string;
+  name: string;
+  version: string;
+  projectName: string;
+  description: string;
+  target: string;
+  data: IDataset;
+  metrics: {
+    items: { [key: string]: IMetric };
+  };
+  configuration: {
+    items: any;
+  };
+  infrastructure: IInfrastructure;
+  code: {
+    commitMessage: string | null;
+  };
+  status: string;
+}
+
+export interface IExperimentData {
+  [key: string]: IExperiment & { checked: boolean };
+}
+
+export interface IExpandExperiment extends IExperiment {
+  data: IExpandDataset;
+}
