982019dc7966835c99dfbbf54b8ebaa0779a603e
Yevhenii <machete325@gmail.com>
2022-12-02 21:53:39 +0200

    feat(mock-data): added mock data
    Added mock data

--- src/Pages/Projects/ProjectPages/Datasets/CategoryDistribution/CategoryDistribution.tsx
+++ src/Pages/Projects/ProjectPages/Datasets/CategoryDistribution/CategoryDistribution.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ToBeImpelemented from '../../../../../components/ToBeImpelemented/ToBeImpelemented';
-import { IDataset } from '../../../../../core/redux/projects/datasets/types';
+import { IDataset } from '../../../../../types/project/Datasets';
+import s from './CategoryDistribution.module.scss';
 
 interface Props {
   data: IDataset;
@@ -10,7 +11,18 @@ function CategoryDistribution({ data }: Props) {
   console.log(data);
   return (
     <div>
-      <ToBeImpelemented element="Category Distribution" color="primary" />
+      <div className={s.mock_container}>
+        <div className={s.title}>item_id distribution</div>
+        <ToBeImpelemented
+          element={(
+            <img
+              alt="descritpion"
+              src="/images/mock/datasets/item-category-distribution.png"
+            />
+          )}
+          color="primary"
+        />
+      </div>
     </div>
   );
 }
