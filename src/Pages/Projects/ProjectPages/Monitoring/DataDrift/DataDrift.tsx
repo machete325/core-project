5f4b142ad30a4fa1a8d9280e9fd79e1573c477e5
Yevhenii <machete325@gmail.com>
2022-12-19 21:59:05 +0200

    feat(monitoring): added logic and UI for monitoring tabs
    Added logic and UI for monitoring tabs

--- src/Pages/Projects/ProjectPages/Monitoring/DataDrift/DataDrift.tsx
+++ src/Pages/Projects/ProjectPages/Monitoring/DataDrift/DataDrift.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import Chart from '../../../../../components/Chart/Chart';
+import Outliers from '../../../../../components/DatasetComponents/Outliers/Outliers';
+import StatisticProperties from '../../../../../components/DatasetComponents/StatisticProperties/StatisticProperties';
+import ToBeImpelemented from '../../../../../components/ToBeImpelemented/ToBeImpelemented';
 import { IMonitoring } from '../../../../../types/project/monitoring';
+import mockData from '../mockData.json';
 import s from './DataDrift.module.scss';
 
 interface Props {
@@ -8,7 +13,66 @@ interface Props {
 
 function DataDrift({ data }: Props) {
   console.log(data);
-  return <div className={s.wrapper}>DataDrift</div>;
+  return (
+    <div className={s.wrapper}>
+      <div className={s.data_container}>
+        <StatisticProperties
+          data={mockData.dataDrift.statistics.statisticProperties}
+          title="Overview of the data used in production"
+        />
+      </div>
+      <div className={s.data_container}>
+        <Outliers data={mockData.dataDrift.statistics.outliers} />
+      </div>
+      {mockData.dataDrift.timeseries.map((item: any) => (
+        <div className={s.data_container}>
+          <div className={s.title}>{`Data Drift ${item.displayName}`}</div>
+          <Chart data={item.subplots} type="monitoring-drift" />
+        </div>
+      ))}
+      {mockData.dataDrift.distributions.map((item: any) => (
+        <div className={s.data_container}>
+          <div className={s.title}>
+            {`Data Distribution ${item.displayName}`}
+            <Chart data={item.subplots} type="monitoring-distribution" />
+          </div>
+        </div>
+      ))}
+      <div className={s.data_container}>
+        <div className={s.title}>item_id</div>
+        <ToBeImpelemented
+          element={
+            <img alt="item-id" src="/images/mock/monitoring/item-id.png" />
+          }
+          color="primary"
+          backgroundBlur
+        />
+      </div>
+      <div className={s.data_container}>
+        <div className={s.title}>item_category_id</div>
+        <ToBeImpelemented
+          element={(
+            <img
+              alt="item-category-id"
+              src="/images/mock/monitoring/item-category-id.png"
+            />
+          )}
+          color="primary"
+          backgroundBlur
+        />
+      </div>
+      <div className={s.data_container}>
+        <div className={s.title}>shop_id</div>
+        <ToBeImpelemented
+          element={
+            <img alt="shop-id" src="/images/mock/monitoring/shop-id.png" />
+          }
+          color="primary"
+          backgroundBlur
+        />
+      </div>
+    </div>
+  );
 }
 
 export default DataDrift;
--- src/Pages/Projects/ProjectPages/Monitoring/Infrastructure/Infrastructure.tsx
+++ src/Pages/Projects/ProjectPages/Monitoring/Infrastructure/Infrastructure.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import Chart from '../../../../../components/Chart/Chart';
+import MachineDetails from '../../../../../components/MachineDetails/MachineDetails';
+import { genMachineChartData } from '../../../../../core/helpers/infrastructureMethods';
 import { IMonitoring } from '../../../../../types/project/monitoring';
+import mockData from '../mockData.json';
 import s from './Infrastructure.module.scss';
 
 interface Props {
@@ -8,7 +12,20 @@ interface Props {
 
 function Infrastructure({ data }: Props) {
   console.log(data);
-  return <div className={s.wrapper}>Infrastructure</div>;
+  return (
+    <div className={s.wrapper}>
+      {mockData.infrastructure.machines.map((machine) => (
+        <div key={machine.id} className={s.data_container}>
+          <MachineDetails orientation="horizontal" data={machine} />
+          <Chart
+            type="infrastructure-line"
+            isFill
+            data={genMachineChartData(machine)}
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Infrastructure;
