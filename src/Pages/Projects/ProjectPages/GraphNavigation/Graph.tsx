b91e6eef72158d8feb84cbfcbde2b0a54e196a05
Yevhenii <machete325@gmail.com>
2023-01-03 20:09:18 +0200

    fix(graph.tsx): fixed arrows curvenes
    Fixed arrows curvenes

--- src/Pages/Projects/ProjectPages/GraphNavigation/Graph.tsx
+++ src/Pages/Projects/ProjectPages/GraphNavigation/Graph.tsx
@@ -9,7 +9,7 @@ import s from './GraphNavigation.module.scss';
 const configuration: { [key: string]: { [key: string]: IConfiguration } } = {
   infrastructure: {
     datasets: {
-      marginLeft: 0.136,
+      marginLeft: 0.128,
       width: 0.116,
       height: 0.641,
       img: '/images/project/graph/dataset.png',
@@ -544,7 +544,15 @@ function Graph({ cardContainerSize, handleGetCardContainerSize }: any) {
             style={{
               width: `${cardContainerSize.width * card.width}px`,
               height: `${cardContainerSize.height * card.height}px`,
-              marginLeft: `${cardContainerSize.width * card.marginLeft}px`,
+              marginLeft: `${
+                key === 'database'
+                || key === 'experiments'
+                || key === 'data_monitoring'
+                || key === 'monitoring'
+                || key === 'model_serving'
+                  ? card.marginLeft
+                  : cardContainerSize.width * card.marginLeft
+              }px`,
               backgroundColor: card.backgroundColor
                 ? card.backgroundColor
                 : 'inherit',
@@ -632,6 +640,7 @@ function Graph({ cardContainerSize, handleGetCardContainerSize }: any) {
               _extendSVGcanvas={item.extendSVGcanvas || 0}
             />
           ))}
+
         <div
           id={`graph-navigation-${key}`}
           className={s.card_container}
@@ -660,10 +669,280 @@ function Graph({ cardContainerSize, handleGetCardContainerSize }: any) {
     );
   };
 
+  const handleFixedArrows = () => {
+    const data: any = {
+      databaseMarginLeft: undefined,
+      experimentsMarginLeft: undefined,
+      dataMonitoringMarginLeft: undefined,
+      modelMonitoringMarginLeft: undefined,
+      modelServingMarginLeft: undefined,
+    };
+
+    // maintenance params
+    const maintenance = document.getElementById('graph-navigation-maintenance');
+    const maintenanceWidth = maintenance?.style.width.replace('px', '');
+    const maintenanceMarginLeft = maintenance?.style.marginLeft.replace(
+      'px',
+      '',
+    );
+
+    // datasets params
+    const datasets = document.getElementById('graph-navigation-datasets');
+    const datasetsWidth = datasets?.style.width.replace('px', '');
+    const datasetsMarginLeft = datasets?.style.marginLeft.replace('px', '');
+
+    // experiments params
+    const experiments = document.getElementById('graph-navigation-experiments');
+    const experimentsWidth = experiments?.style.width.replace('px', '');
+    // const experimentsMarginLeft = experiments?.style.marginLeft.replace(
+    //   'px',
+    //   '',
+    // );
+
+    // database params
+    const database = document.getElementById('graph-navigation-database');
+    const databaseWidth = database?.style.width.replace('px', '');
+    const databaseMarginLeft = database?.style.marginLeft.replace('px', '');
+
+    // data labeling (web) params
+    const dataLabeling = document.getElementById(
+      'graph-navigation-data_labeling_web',
+    );
+    const dataLabelingWidth = dataLabeling?.style.width.replace('px', '');
+    const dataLabelingMarginLeft = dataLabeling?.style.marginLeft.replace(
+      'px',
+      '',
+    );
+
+    // data exploration params
+    const dataExploration = document.getElementById(
+      'graph-navigation-data_exploration',
+    );
+    const dataExplorationWidth = dataExploration?.style.width.replace('px', '');
+    const dataExplorationMarginLeft = dataExploration?.style.marginLeft.replace(
+      'px',
+      '',
+    );
+
+    // ML Model params
+    const mlModel = document.getElementById('graph-navigation-ml_model');
+    const mlModelWidth = mlModel?.style.width.replace('px', '');
+    const mlModelMarginLeft = mlModel?.style.marginLeft.replace('px', '');
+
+    // Evaluation params
+    const evaluation = document.getElementById('graph-navigation-evaluation');
+    const evaluationMarginLeft = evaluation?.style.marginLeft.replace('px', '');
+
+    // Data stream params
+    const dataStream = document.getElementById('graph-navigation-api_data');
+    const dataStreamWidth = dataStream?.style.width.replace('px', '');
+    const dataStreamMarginLeft = dataStream?.style.marginLeft.replace('px', '');
+
+    // Data monitoring params
+    const dataMonitoring = document.getElementById(
+      'graph-navigation-data_monitoring',
+    );
+    const dataMonitoringWidth = dataMonitoring?.style.width.replace('px', '');
+    const dataMonitoringMarginLeft = dataMonitoring?.style.marginLeft.replace(
+      'px',
+      '',
+    );
+
+    // Model monitoring params
+    const modelMonitoring = document.getElementById(
+      'graph-navigation-monitoring',
+    );
+    const modelMonitoringWidth = modelMonitoring?.style.width.replace('px', '');
+    // const modelMonitoringMarginLeft = modelMonitoring?.style.marginLeft.replace(
+    //   'px',
+    //   '',
+    // );
+
+    // keys params
+    const infrastructureKey = document.getElementById(
+      'graph-infrastructure-key',
+    );
+    const infrastructureKeyWidth = infrastructureKey?.clientWidth;
+    const researchKey = document.getElementById('graph-research-key');
+    const researchKeyWidth = researchKey?.clientWidth;
+    const productionKey = document.getElementById('graph-production-key');
+    const productionKeyWidth = productionKey?.clientWidth;
+
+    // database
+    if (
+      maintenanceWidth
+      && maintenanceMarginLeft
+      && infrastructureKeyWidth
+      && researchKeyWidth
+      && datasetsMarginLeft
+      && datasetsWidth
+      && databaseWidth
+    ) {
+      const updatedDatabaseMarginLeft = parseFloat(datasetsMarginLeft)
+        + (infrastructureKeyWidth - researchKeyWidth)
+        - (parseFloat(maintenanceWidth) + parseFloat(maintenanceMarginLeft))
+        + (parseFloat(datasetsWidth) - parseFloat(databaseWidth)) / 2
+        + 0.2;
+      data.databaseMarginLeft = updatedDatabaseMarginLeft;
+    }
+
+    // experiments
+    if (
+      maintenanceWidth
+      && maintenanceMarginLeft
+      && infrastructureKeyWidth
+      && researchKeyWidth
+      && datasetsMarginLeft
+      && datasetsWidth
+      && databaseWidth
+      && databaseMarginLeft
+      && dataLabelingWidth
+      && dataLabelingMarginLeft
+      && dataExplorationWidth
+      && dataExplorationMarginLeft
+      && mlModelMarginLeft
+      && mlModelWidth
+      && experimentsWidth
+    ) {
+      const keyDifference = infrastructureKeyWidth - researchKeyWidth;
+      const updatedExperimentsMarginLeft = parseFloat(maintenanceWidth)
+        + parseFloat(maintenanceMarginLeft)
+        + parseFloat(databaseWidth)
+        + parseFloat(data.databaseMarginLeft)
+        + parseFloat(dataLabelingWidth)
+        + parseFloat(dataLabelingMarginLeft)
+        + parseFloat(dataExplorationWidth)
+        + parseFloat(dataExplorationMarginLeft)
+        + parseFloat(mlModelMarginLeft)
+        - keyDifference
+        - (parseFloat(datasetsWidth) + parseFloat(datasetsMarginLeft))
+        + (parseFloat(mlModelWidth) - parseFloat(experimentsWidth)) / 2
+        - 0.2;
+      data.experimentsMarginLeft = updatedExperimentsMarginLeft;
+    }
+
+    // data monitoring
+    if (
+      maintenanceWidth
+      && maintenanceMarginLeft
+      && productionKeyWidth
+      && researchKeyWidth
+      && databaseWidth
+      && databaseMarginLeft
+      && dataLabelingWidth
+      && dataLabelingMarginLeft
+      && dataStreamWidth
+      && dataStreamMarginLeft
+      && dataExplorationMarginLeft
+      && dataMonitoringWidth
+      && dataExplorationWidth
+    ) {
+      const keyDifference = productionKeyWidth - researchKeyWidth;
+      const updatedDataMonitoringMarginLeft = parseFloat(maintenanceWidth)
+        + parseFloat(maintenanceMarginLeft)
+        + parseFloat(databaseWidth)
+        + parseFloat(data.databaseMarginLeft)
+        + parseFloat(dataLabelingWidth)
+        + parseFloat(dataLabelingMarginLeft)
+        + parseFloat(dataExplorationMarginLeft)
+        - keyDifference
+        - (parseFloat(dataStreamWidth) + parseFloat(dataStreamMarginLeft));
+      data.dataMonitoringMarginLeft = updatedDataMonitoringMarginLeft + 0.2;
+    }
+
+    // model monitoring
+    if (
+      maintenanceWidth
+      && maintenanceMarginLeft
+      && productionKeyWidth
+      && researchKeyWidth
+      && databaseWidth
+      && databaseMarginLeft
+      && dataLabelingWidth
+      && dataLabelingMarginLeft
+      && dataStreamWidth
+      && dataStreamMarginLeft
+      && dataExplorationMarginLeft
+      && dataMonitoringWidth
+      && dataExplorationWidth
+      && mlModelMarginLeft
+      && mlModelWidth
+      && modelMonitoringWidth
+      && dataMonitoringMarginLeft
+    ) {
+      const keyDifference = productionKeyWidth - researchKeyWidth;
+      const updatedModelMonitoringMarginLeft = parseFloat(maintenanceWidth)
+        + parseFloat(maintenanceMarginLeft)
+        + parseFloat(databaseWidth)
+        + parseFloat(data.databaseMarginLeft)
+        + parseFloat(dataLabelingWidth)
+        + parseFloat(dataLabelingMarginLeft)
+        + parseFloat(dataExplorationMarginLeft)
+        + parseFloat(dataExplorationWidth)
+        + parseFloat(mlModelMarginLeft)
+        - keyDifference
+        - (parseFloat(dataStreamWidth)
+          + parseFloat(dataStreamMarginLeft)
+          + parseFloat(dataMonitoringWidth)
+          + parseFloat(data.dataMonitoringMarginLeft))
+        + (parseFloat(mlModelWidth) - parseFloat(modelMonitoringWidth)) / 2;
+
+      data.modelMonitoringMarginLeft = updatedModelMonitoringMarginLeft + 0.2;
+    }
+
+    // model serving
+    if (
+      maintenanceWidth
+      && maintenanceMarginLeft
+      && productionKeyWidth
+      && researchKeyWidth
+      && databaseWidth
+      && databaseMarginLeft
+      && dataLabelingWidth
+      && dataLabelingMarginLeft
+      && dataStreamWidth
+      && dataStreamMarginLeft
+      && dataExplorationMarginLeft
+      && dataMonitoringWidth
+      && dataExplorationWidth
+      && mlModelMarginLeft
+      && mlModelWidth
+      && modelMonitoringWidth
+      && dataMonitoringMarginLeft
+      && evaluationMarginLeft
+    ) {
+      const keyDifference = productionKeyWidth - researchKeyWidth;
+      const updatedModelServingMarginLeft = parseFloat(maintenanceWidth)
+        + parseFloat(maintenanceMarginLeft)
+        + parseFloat(databaseWidth)
+        + parseFloat(data.databaseMarginLeft)
+        + parseFloat(dataLabelingWidth)
+        + parseFloat(dataLabelingMarginLeft)
+        + parseFloat(dataExplorationMarginLeft)
+        + parseFloat(dataExplorationWidth)
+        + parseFloat(mlModelMarginLeft)
+        + parseFloat(mlModelWidth)
+        + parseFloat(evaluationMarginLeft)
+        - keyDifference
+        - (parseFloat(dataStreamWidth)
+          + parseFloat(dataStreamMarginLeft)
+          + parseFloat(dataMonitoringWidth)
+          + parseFloat(data.dataMonitoringMarginLeft)
+          + parseFloat(modelMonitoringWidth)
+          + parseFloat(data.modelMonitoringMarginLeft));
+
+      data.modelServingMarginLeft = updatedModelServingMarginLeft + 0.2;
+    }
+
+    return data;
+  };
+
   const generateMarkup = (config: any) => {
     const jsx = Object.keys(config).map((key) => (
       <div id="graph-content" key={key} className={s.graph_content}>
-        <div className={s.status_tag_container}>{key}</div>
+        <div id={`graph-${key}-key`} className={s.status_tag_container}>
+          {key}
+        </div>
         <div className={s.card_wrapper}>
           {Object.keys(config[key]).map((item) => handleCreateCard(config[key][item], item))}
         </div>
@@ -671,6 +950,23 @@ function Graph({ cardContainerSize, handleGetCardContainerSize }: any) {
     ));
     return jsx;
   };
+
+  useEffect(() => {
+    const {
+      databaseMarginLeft,
+      experimentsMarginLeft,
+      dataMonitoringMarginLeft,
+      modelMonitoringMarginLeft,
+      modelServingMarginLeft,
+    }: any = handleFixedArrows();
+    const cardConfigurationCopy = JSON.parse(JSON.stringify(cardConfiguration));
+    cardConfigurationCopy.research.database.marginLeft = databaseMarginLeft;
+    cardConfigurationCopy.infrastructure.experiments.marginLeft = experimentsMarginLeft;
+    cardConfigurationCopy.production.data_monitoring.marginLeft = dataMonitoringMarginLeft;
+    cardConfigurationCopy.production.monitoring.marginLeft = modelMonitoringMarginLeft;
+    cardConfigurationCopy.production.model_serving.marginLeft = modelServingMarginLeft;
+    setCardConfiguration(cardConfigurationCopy);
+  }, [cardContainerSize]);
 
   const markup = useMemo(
     () => generateMarkup(cardConfiguration),
